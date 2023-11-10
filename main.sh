17c6e0518c5e57d760c398f573c7c12ea0a3ad9e
Dream Maker <67145585+KawaCoder@users.noreply.github.com>
2023-11-10 16:37:10 +0100

    Update main.sh

--- main.sh
+++ main.sh
@@ -20,17 +20,17 @@ function show_help() {
     echo "osalt [options] [script_name]"
     echo
     echo "Options:"
-    echo "  -s, --startup <script_name>    Add a startup script."
-    echo "  -w, --shutdown <script_name>   Add a shutdown script."
-    echo "  -c, --scheduled <script_name>  Add a scheduled script."
-    echo "  -d, --delete <script_name>     Delete a script."
-    echo "  -l, --list                     List all available scripts."
-    echo "  -h, --help                     Prints this help."
+    echo "  -s, --startup <script_name>                Add a startup script."
+    echo "  -w, --shutdown <script_name>               Add a shutdown script."
+    echo "  -c, --scheduled <script_name> <scheduled>  Add a scheduled script."
+    echo "  -d, --delete <script_name>                 Delete a script."
+    echo "  -l, --list                                 List all available scripts."
+    echo "  -h, --help                                 Prints this help."
     echo
     echo "Examples:"
     echo "  osalt -s my_startup_script.sh"
     echo "  osalt -h my_shutdown_script.sh"
-    echo "  osalt -c my_scheduled_script.sh"
+    echo "  osalt -c my_scheduled_script.sh \"*-*-* 9-17:00:00\""
     echo "  osalt -v my_startup_script.sh"
     echo "  osalt -d my_shutdown_script.sh"
     echo "  osalt -l"
@@ -62,20 +62,37 @@ function delete_script() {
     just_file_name="$(basename "$script_name" ".sh")"
     config_file="config.json"
 
-    if [ -f "config.json" ]; then
-        script_category=""
-        if jq -r ".shutdown[] | select(.name == \"$script_name\")" "$config_file" > /dev/null; then
-            script_category="shutdown"
-        elif jq -r ".startup[] | select(.name == \"$script_name\")" "$config_file" > /dev/null; then
-            script_category="startup"
-        elif jq -r ".scheduled[] | select(.name == \"$script_name\")" "$config_file" > /dev/null; then
-            script_category="scheduled"
-        else
-            echo "Script \"$script_name\" not found in config.json"
-            exit 1
-        fi
+#    if [ -f "config.json" ]; then
+#        script_category=""
+#        if [ "$(jq -r ".startup | index( \"$script_name\" )" "$config_file")" != null ]; then
+#            script_category="startup"
+#        elif [ "$(jq -r ".shutdown | index( \"$script_name\" )" "$config_file")" != null ]; then
+#            script_category="shutdown"
+#        elif [ "$(jq -r ".scheduled | index( \"$script_name\" )" "$config_file")" != null ]; then
+#            script_category="scheduled"
+#        else
+#            echo "Script \"$script_name\" not found in config.json"
+#            exit 1
+#        fi
+        script_category=$(jq -r '
+  if .startup[] | select(.name == "test.sh") then
+    "startup"
+  else
+    "startup not found"
+  end,
+  if .shutdown[] | select(.name == "test.sh") then
+    "shutdown"
+  else
+    "shutdown not found"
+  end,
+  if .scheduled[] | select(.name == "test.sh") then
+    "scheduled"
+  else
+    "scheduled not found"
+  end' config.json)
 
-        jq ".$script_category = [.${script_category}[] | select(.name != \"$script_name\")]" "$config_file" > temp.json
+        jq --arg script_name "$script_name" 'del(.startup[] | select(.name == $script_name)) | del(.shutdown[] | select(.name == $script_name)) | del(.scheduled[] | select(.name == $script_name))' "config.json" > temp.json
+
         mv temp.json "$config_file"
 
         if [ -f "$HOME/OSALT/scripts/$script_category/$script_name" ]; then
@@ -85,12 +102,16 @@ function delete_script() {
         fi
 
         echo "Script \"$script_name\" has been deleted."
-    else
-        echo "config.json file not found."
-        exit 1
-   
-   fi
 
+        if [ "$script_category" = "scheduled" ]; then
+            systemctl --user stop "$just_file_name".timer
+            systemctl --user disable "$just_file_name".timer
+            echo "$HOME/.config/systemd/user/$just_file_name.timer"
+            rm "$HOME/.config/systemd/user/$just_file_name.timer"
+            echo "Systemd user service file removed."
+        
+        fi
+        
         systemctl --user stop "$just_file_name".service
         systemctl --user disable "$just_file_name".service
         echo "$HOME/.config/systemd/user/$just_file_name.service"
@@ -99,10 +120,12 @@ function delete_script() {
 
 }
 
+
 function add_script(){
     new_script_path=$1
     new_script_name=$(basename "$new_script_path")
     option="$2"
+    schedule="$3"
 
     if [ -f "config.json" ]; then
         if grep -q "\"name\": \"$new_script_name\"" config.json; then
@@ -143,6 +166,7 @@ ExecStart=$HOME/OSALT/scripts/$option/$new_script_name
 WantedBy=default.target
         
         " >> "$HOME"/.config/systemd/user/"$just_file_name".service
+    systemctl --user enable "$just_file_name".service
 
     elif [ "$option" = "shutdown" ]; then
     echo "
@@ -158,12 +182,37 @@ ExecStart=$HOME/OSALT/scripts/$option/$new_script_name
 [Install]
 WantedBy=default.target
 " >> "$HOME"/.config/systemd/user/"$just_file_name".service
+    systemctl --user enable "$just_file_name".service
+
+    elif [ "$option" = "scheduled" ]; then
+        echo "
+[Unit]
+Description=$just_file_name
+
+[Service]
+Type=oneshot
+ExecStart=$HOME/OSALT/scripts/$option/$new_script_name
+
+" >> "$HOME"/.config/systemd/user/"$just_file_name".service
+
+        echo "
+[Unit]
+Description=$just_file_name
+
+[Timer]
+OnCalendar=$schedule
+Persistent=true
+
+[Install]
+WantedBy=timers.target
+" >> "$HOME"/.config/systemd/user/"$just_file_name".timer
+    systemctl --user enable "$just_file_name".timer
 
     fi
-    systemctl --user enable "$just_file_name".service
-        
-}
 
+}
+#systemd-analyze verify ~/.config/systemd/user/test.timer
+#systemd-analyze verify ~/.config/systemd/user/test.service
 if [ "$#" -lt 1 ]; then
     show_help
     exit 1
@@ -189,8 +238,9 @@ while [ "$#" -gt 0 ]; do
         -c|--scheduled)
             file="$2"
             option="scheduled"
-            add_script "$file" "$option"
-            shift 2
+            schedule="$3"
+            add_script "$file" "$option" "$3"
+            shift 3
             ;;
         
         -d|--delete)
